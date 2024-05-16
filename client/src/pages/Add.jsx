09535e2c138b18b65ca495632d524ff01444d038
Devanshi Sharma <21ucc022@lnmiit.ac.in>
2024-05-16 23:44:50 +0530

    first commit

---
+++ client/src/pages/Add.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Add = () => {
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  //   console.log(book);
+
+  // this function is called when new entry is created and send this to backend
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8800/books", book);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="form">
+      <h1>Add a book</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="description"
+        onChange={handleChange}
+        name="desc"
+      />
+      <input
+        type="number"
+        placeholder="price"
+        onChange={handleChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handleChange}
+        name="cover"
+      />
+
+      <button className="formButton" onClick={handleClick}>
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default Add;
